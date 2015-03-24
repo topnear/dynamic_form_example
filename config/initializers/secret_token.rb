7f4d491e77f736cb21e4faef3b5a73f62a42e7ca
topgrammer <topgrammer@gmail.com>
2015-03-24 22:21:15 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DynamicForm::Application.config.secret_key_base = '9ee232823985de0a16e23f08cebe21dc5efa92e5741c9ad182d6b4d0d69cff3509ecc25f76abf862a2cb5feea4fe83ab3052777e84de0e0e03981fc3e4a99cdf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DynamicForm::Application.config.session_store :cookie_store, key: '_dynamic_form_session'
