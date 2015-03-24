bdd8e1f18cf1626f56974e906ffd7c62b61e1ac4
topgrammer <topgrammer@gmail.com>
2015-03-24 23:09:33 +0800

    implement simple CRUD

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,22 +1,47 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.all
   end
 
   def show
+    @activity = Activity.find(params[:id])
   end
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+
+    if @activity.save!
+      redirect_to @activity
+    end
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
 
   def update
+    @activity = Activity.find(params[:id])
+    if @activity.update_attributes(activity_params)
+      redirect_to @activity
+    end
   end
 
   def destroy
+    @activity = Activity.find(params[:id])
+    if @activity.destroy
+      redirect_to activities_path
+    end
   end
+
+ 
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title)
+  end
+
 end
